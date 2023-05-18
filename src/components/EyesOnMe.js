a0e21bb8e8feead64100a6ad173cf88d59de38de
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-05-18 14:19:46 +0300

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+  return (
+    <div>
+        <button onFocus={()=>console.log("Good!")} onBlur={()=>console.log('Hey! Eyes on me!')}>Eyes On Me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
